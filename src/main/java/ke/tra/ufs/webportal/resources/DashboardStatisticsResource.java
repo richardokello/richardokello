9dd27b4697039663dd9e44951bf3c82d41d8f75c
owori.juma@tracom.co.ke <tracom1234>
2019-09-03 18:55:36 +0300

    latest

--- src/main/java/ke/tra/ufs/webportal/resources/DashboardStatisticsResource.java
+++ src/main/java/ke/tra/ufs/webportal/resources/DashboardStatisticsResource.java
@@ -1,10 +1,7 @@
 package ke.tra.ufs.webportal.resources;
 
 import ke.axle.chassis.wrappers.ResponseWrapper;
-<<<<<<< HEAD
-=======
 import ke.tra.ufs.webportal.entities.wrapper.DashboardItemsWrapper;
->>>>>>> 23f67ebf5389b1cda1f0ffbf53570b86cc3801e6
 import ke.tra.ufs.webportal.service.DashboardStatisticService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +13,8 @@ public class DashboardStatisticsResource {
 
     private final DashboardStatisticService dashboardStatisticService;
 
-    DashboardStatisticsResource(DashboardStatisticService dashboardStatisticService){
-       this.dashboardStatisticService = dashboardStatisticService;
+    DashboardStatisticsResource(DashboardStatisticService dashboardStatisticService) {
+        this.dashboardStatisticService = dashboardStatisticService;
     }
 
     @RequestMapping(path = "/webportal-stats", method = RequestMethod.GET)
