492f8aa6717566d810df182598e4fb07e21386d5
Kenneth Mwangi <kmwangi@tracom.co.ke>
2020-10-02 19:04:56 +0300

    dashboard stats

--- src/main/java/ke/tra/ufs/webportal/service/template/DashboardStatisticServiceTemplate.java
+++ src/main/java/ke/tra/ufs/webportal/service/template/DashboardStatisticServiceTemplate.java
@@ -82,7 +82,8 @@ public class DashboardStatisticServiceTemplate implements DashboardStatisticServ
         single.add(new DashboardItemsWrapper("Total Bank Branches", getTotalBankBranches(AppConstants.NO), "/agency-webportal/bank-branches"));
         single.add(new DashboardItemsWrapper("Total Bank Regions", getTotalBankRegions(AppConstants.NO), "/agency-webportal/bank-regions"));
         single.add(new DashboardItemsWrapper("Agents Assigned Device", getTotalAssignedDeviceAgents(AppConstants.NO), "/agency-webportal/device-management/assigned-devices-list"));
-        single.add(new DashboardItemsWrapper("Total Outlets", getTotalOutlets(AppConstants.NO), "/agency-webportal/customer-outlets"));
+//        single.add(new DashboardItemsWrapper("Total Outlets", getTotalOutlets(AppConstants.NO), "/agency-webportal/customer-outlets"));
+        single.add(new DashboardItemsWrapper("Total Outlets", getTotalOutlets(AppConstants.NO), ""));
         //single.add(new DashboardItemsWrapper("Agent Supervisors", getTotalTypeUsers(AppConstants.USER_TYPE_AGENT_SUPERVISOR, AppConstants.NO), "/common-modules/users/agent-supervisors"));
         single.add(new DashboardItemsWrapper("BackOffice Users", getTotalTypeUsers(AppConstants.USER_TYPE_BACKOFFICE_USER, AppConstants.NO), "/common-modules/users/back-office"));
         //single.add(new DashboardItemsWrapper("Head Of Distribution", getTotalTypeUsers(AppConstants.USER_TYPE_HEAD_OF_DISTRIBUTION, AppConstants.NO), "/common-modules/users/hod"));
