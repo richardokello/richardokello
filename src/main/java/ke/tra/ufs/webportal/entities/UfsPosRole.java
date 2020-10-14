5a7a92088f8ee75ebc8d006b9728e185d733eb6e
Kenneth Mwangi <kmwangi@tracom.co.ke>
2020-10-14 14:57:27 +0300

    adding pos role

---
+++ src/main/java/ke/tra/ufs/webportal/entities/UfsPosRole.java
@@ -0,0 +1,67 @@
+package ke.tra.ufs.webportal.entities;
+
+import ke.axle.chassis.annotations.Filter;
+import ke.axle.chassis.annotations.ModifiableField;
+import ke.axle.chassis.annotations.Searchable;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "UFS_POS_ROLE")
+@Data
+public class UfsPosRole implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @GenericGenerator(
+            name = "UFS_POS_ROLE_SEQ",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "UFS_POS_ROLE_SEQ"),
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
+                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
+            }
+    )
+    @GeneratedValue(generator = "UFS_POS_ROLE_SEQ")
+    @Column(name = "ID")
+    private Long id;
+
+    @Size(max = 30)
+    @Filter
+    @Searchable
+    @ModifiableField
+    @Column(name = "POS_ROLE_NAME")
+    private String posRoleName;
+
+    @Size(max = 30)
+    @Filter
+    @Searchable
+    @ModifiableField
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "CREATED_AT",insertable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+    @Size(max = 20)
+    @Filter
+    @Searchable
+    @Column(name = "ACTION", insertable = false)
+    private String action;
+    @Size(max = 20)
+    @Filter
+    @Searchable
+    @Column(name = "ACTION_STATUS", insertable = false)
+    private String actionStatus;
+    @Size(max = 3)
+    @Filter
+    @Searchable
+    @Column(name = "INTRASH", insertable = false)
+    private String intrash;
+}
---
+++ src/main/java/ke/tra/ufs/webportal/resources/UfsPosRoleController.java
@@ -0,0 +1,47 @@
+package ke.tra.ufs.webportal.resources;
+
+import ke.axle.chassis.ChasisResource;
+import ke.axle.chassis.utils.AppConstants;
+import ke.axle.chassis.utils.LoggerService;
+import ke.axle.chassis.wrappers.ResponseWrapper;
+import ke.tra.ufs.webportal.entities.UfsEdittedRecord;
+import ke.tra.ufs.webportal.entities.UfsPosRole;
+import ke.tra.ufs.webportal.repository.UfsPosRoleRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+import javax.validation.Valid;
+
+
+@RestController
+@RequestMapping("/pos-role")
+public class UfsPosRoleController extends ChasisResource<UfsPosRole, UfsEdittedRecord,Long> {
+
+    private final UfsPosRoleRepository posRoleRepository;
+
+    public UfsPosRoleController(LoggerService loggerService, EntityManager entityManager,
+                                UfsPosRoleRepository posRoleRepository) {
+        super(loggerService, entityManager);
+        this.posRoleRepository = posRoleRepository;
+    }
+
+    @Override
+    @Transactional
+    public ResponseEntity<ResponseWrapper<UfsPosRole>> create(@Valid @RequestBody UfsPosRole ufsPosRole) {
+        ResponseWrapper responseWrapper = new ResponseWrapper();
+        UfsPosRole posRole = posRoleRepository.findByPosRoleNameAndIntrash(ufsPosRole.getPosRoleName(), AppConstants.NO);
+
+        if (posRole != null ) {
+            responseWrapper.setCode(HttpStatus.CONFLICT.value());
+            responseWrapper.setMessage(ufsPosRole.getPosRoleName()+" Role Name already exist");
+
+            return new ResponseEntity(responseWrapper, HttpStatus.CONFLICT);
+        }
+        return super.create(posRole);
+    }
+}
