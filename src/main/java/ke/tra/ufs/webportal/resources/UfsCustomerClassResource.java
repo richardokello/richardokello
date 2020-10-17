c87e2d78b81cc3c96421197b6281ab63488a5037
Kenneth Mwangi <kmwangi@tracom.co.ke>
2020-10-17 19:03:21 +0300

    bank regions and customer class update

--- src/main/java/ke/tra/ufs/webportal/resources/UfsCustomerClassResource.java
+++ src/main/java/ke/tra/ufs/webportal/resources/UfsCustomerClassResource.java
@@ -1,20 +1,47 @@
 package ke.tra.ufs.webportal.resources;
 
 import ke.axle.chassis.ChasisResource;
+import ke.axle.chassis.utils.AppConstants;
 import ke.axle.chassis.utils.LoggerService;
+import ke.axle.chassis.wrappers.ResponseWrapper;
 import ke.tra.ufs.webportal.entities.UfsCustomerClass;
 import ke.tra.ufs.webportal.entities.UfsEdittedRecord;
+import ke.tra.ufs.webportal.repository.UfsCustomerClassRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.persistence.EntityManager;
+import javax.validation.Valid;
 
 
 @Controller
 @RequestMapping("/customer-class")
 public class UfsCustomerClassResource extends ChasisResource<UfsCustomerClass, Long, UfsEdittedRecord> {
 
-    public UfsCustomerClassResource(LoggerService loggerService, EntityManager entityManager) {
+    private final UfsCustomerClassRepository customerClassRepository;
+
+    public UfsCustomerClassResource(LoggerService loggerService, EntityManager entityManager,
+                                    UfsCustomerClassRepository customerClassRepository) {
         super(loggerService, entityManager);
+        this.customerClassRepository = customerClassRepository;
+    }
+
+    @Override
+    @Transactional
+    public ResponseEntity<ResponseWrapper<UfsCustomerClass>> create(@Valid @RequestBody UfsCustomerClass ufsCustomerClass) {
+        ResponseWrapper responseWrapper = new ResponseWrapper();
+        UfsCustomerClass customerClass = customerClassRepository.findByNameAndIntrash(ufsCustomerClass.getName(), AppConstants.NO);
+        if (customerClass != null ) {
+            responseWrapper.setCode(HttpStatus.CONFLICT.value());
+            responseWrapper.setMessage(ufsCustomerClass.getName()+" Customer Class Name already exist");
+
+            return new ResponseEntity(responseWrapper, HttpStatus.CONFLICT);
+        }
+
+        return super.create(ufsCustomerClass);
     }
 }
