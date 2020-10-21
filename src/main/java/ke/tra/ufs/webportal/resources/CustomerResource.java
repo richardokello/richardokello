79e352a1dbb7d6c1eb814bb0569fc82a311f48fc
Evans Kangethe <ekangethe@tracom.co.ke>
2020-10-21 16:00:51 +0300

    fix

--- src/main/java/ke/tra/ufs/webportal/resources/CustomerResource.java
+++ src/main/java/ke/tra/ufs/webportal/resources/CustomerResource.java
@@ -363,8 +363,6 @@ public class CustomerResource extends ChasisResource<UfsCustomer, Long, UfsEditt
 
                     }
 
-
-
                 }else {
 
                     loggerService.log("Failed To Approve Customer",
