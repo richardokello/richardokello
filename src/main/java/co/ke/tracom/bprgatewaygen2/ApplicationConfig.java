4be08906bc3d24c850d9be91eb9347e85fc265e8
John Mulongo <john.mulongo@tracom.co.ke>
2020-12-01 15:37:50 +0300

    Create a Rest Template bean to be used application wide

---
+++ src/main/java/co/ke/tracom/bprgatewaygen2/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package co.ke.tracom.bprgatewaygen2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationConfig {
+    /**
+     * Create rest template bean to be used application wide
+     * @return rest template instance
+     */
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
