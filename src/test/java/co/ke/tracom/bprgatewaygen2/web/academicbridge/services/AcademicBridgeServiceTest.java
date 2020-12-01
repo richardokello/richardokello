5b14011705c61aab76558a72c90ca54c9840eaee
John Mulongo <john.mulongo@tracom.co.ke>
2020-12-01 15:40:53 +0300

    Complete academic-bridge integration test: 1 failing

--- src/test/java/co/ke/tracom/bprgatewaygen2/web/academicbridge/services/AcademicBridgeServiceTest.java
+++ src/test/java/co/ke/tracom/bprgatewaygen2/web/academicbridge/services/AcademicBridgeServiceTest.java
@@ -1,6 +1,10 @@
 package co.ke.tracom.bprgatewaygen2.web.academicbridge.services;
 
 import co.ke.tracom.bprgatewaygen2.core.util.CustomObjectMapper;
+import co.ke.tracom.bprgatewaygen2.web.academicbridge.data.AcademicBridgeResponse;
+import co.ke.tracom.bprgatewaygen2.web.academicbridge.data.paymentstatus.AcademicBridgePaymentStatusResponse;
+import co.ke.tracom.bprgatewaygen2.web.academicbridge.data.paymentstatus.PaymentStatusRequest;
+import co.ke.tracom.bprgatewaygen2.web.academicbridge.data.savepayment.SavePaymentRequest;
 import co.ke.tracom.bprgatewaygen2.web.academicbridge.data.studentdetails.GetStudentDetails;
 import co.ke.tracom.bprgatewaygen2.web.academicbridge.data.studentdetails.GetStudentDetailsResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -30,7 +34,7 @@ import static org.junit.Assert.assertTrue;
 public class AcademicBridgeServiceTest {
 
     @ClassRule
-    public static WireMockRule wireMockRule = new WireMockRule(8900);
+    public static WireMockRule wireMockRule = new WireMockRule(8900); // start mock server at port 8900
 
     @Autowired
     AcademicBridgeService academicBridgeService;
@@ -38,7 +42,7 @@ public class AcademicBridgeServiceTest {
     ObjectMapper objectMapper = new CustomObjectMapper();
 
     @Value("${academic-bridge.base.url.test}")
-    private String baseUrl;
+    private String baseUrl; // set base url to mock server's url
 
     @Value("${academic-bridge.api-key}")
     private String academicBridgeAPIKey;
@@ -53,9 +57,9 @@ public class AcademicBridgeServiceTest {
     @Value("${academic-bridge.save-payment-url}%s?" +
             "API_KEY=%s&" +
             "API_SECRET=%s&" +
-            "reference_number=%s&" +
+            "reference_number=%s" +
             "paid_amount=%s&" +
-            "sender_name=%&" +
+            "sender_name=%s&" +
             "sender_phone_number=%s&" +
             "reason=%s"
     )
@@ -75,40 +79,93 @@ public class AcademicBridgeServiceTest {
     @Test
     public void fetchStudentDetailsByBillNumber() throws JsonProcessingException {
         GetStudentDetailsResponse expectedResponse = generateStudentDetailsResponse();
-        String expectedResponseString = objectMapper.writeValueAsString(expectedResponse);
         GetStudentDetails userDetailsRequest = generateStudentDetailsRequest();
+        String studentDetailsUrl = generateGetStudentDetailsUrl(userDetailsRequest);
+        String expectedResponseString = objectMapper.writeValueAsString(expectedResponse);
 
-        String url = generateGetStudentDetailsUrl(userDetailsRequest);
-
-        // Mock of Academic Bridge API
-        wireMockRule.stubFor(WireMock.get(WireMock.urlPathEqualTo(url))
-                .willReturn(WireMock.aResponse()
-                        .withBody(expectedResponseString)
-                        .withStatus(HttpStatus.OK.value())
-                        .withHeader("Content-Type",
-                                "application/json;charset=UTF-8")));
+        configureWireMockForOkResponse(expectedResponseString, studentDetailsUrl);
 
         // Response from remote API
-        ResponseEntity<GetStudentDetailsResponse> response = academicBridgeService.fetchStudentDetailsByBillNumber(userDetailsRequest);
+        GetStudentDetailsResponse response = academicBridgeService.fetchStudentDetailsByBillNumber(userDetailsRequest);
         assertNotNull(response);
-        assertTrue(response.getBody() instanceof GetStudentDetailsResponse);
+        assertTrue(response instanceof GetStudentDetailsResponse);
 
         GetStudentDetailsResponse actualResponse = generateStudentDetailsResponse();
         assertNotNull(actualResponse);
 
-        // Both response should match
+        // Both responses should match
         assertGetStudentResponseDetails(expectedResponse, actualResponse);
     }
 
     @Test
-    public void sendPaymentDetailsToAcademicBridge() {
+    public void sendPaymentDetailsToAcademicBridge() throws JsonProcessingException {
+        AcademicBridgeResponse expectedResponse = generateSendPaymentResponse();
+        SavePaymentRequest savePaymentRequest = generateSavePaymentRequestObj();
+        String savePaymentUrl = generateSavePaymentRequestUrl(savePaymentRequest);
+        String expectedResponseString = objectMapper.writeValueAsString(expectedResponse);
+
+        configureWireMockForOkResponse(expectedResponseString, savePaymentUrl);
+
+        // Response from remote API
+        AcademicBridgeResponse response = academicBridgeService.sendPaymentDetailsToAcademicBridge(savePaymentRequest);
+        assertNotNull(response);
+        assertTrue(response instanceof AcademicBridgeResponse);
+
+        // Both responses should match
+        assertSendPaymentResponse(expectedResponse, response);
     }
 
     @Test
-    public void checkPaymentStatus() {
+    public void checkPaymentStatus() throws JsonProcessingException {
+        AcademicBridgePaymentStatusResponse expectedResponse = generatePaymentStatusResponse();
+        PaymentStatusRequest paymentStatusRequest = generatePaymentStatusRequest();
+        String savePaymentUrl = generatePaymentStatusRequestUrl(paymentStatusRequest);
+        String expectedResponseString = objectMapper.writeValueAsString(expectedResponse);
+
+        configureWireMockForOkResponse(expectedResponseString, savePaymentUrl);
+
+        // Response from remote API
+        AcademicBridgePaymentStatusResponse response = academicBridgeService.checkPaymentStatus(
+                paymentStatusRequest);
+        assertNotNull(response);
+        assertTrue(response instanceof AcademicBridgePaymentStatusResponse);
+
+        // Both responses should match
+        assertPaymentStatusResponse(expectedResponse, response);
     }
 
-    // Sample response data
+    private void assertPaymentStatusResponse(AcademicBridgePaymentStatusResponse expected, AcademicBridgePaymentStatusResponse actual) {
+        assertEquals(expected.getError_code(), actual.getError_code());
+        assertEquals(expected.getError_msg(), actual.getError_msg());
+        assertEquals(expected.getReference_number(), actual.getReference_number());
+        assertEquals(expected.getAb_reference(), actual.getAb_reference());
+    }
+
+    private String generatePaymentStatusRequestUrl(PaymentStatusRequest request) {
+        String requestURL = String.format(checkPaymentStatusURL,
+                request.getReferenceNo(),
+                academicBridgeAPIKey,
+                academicBridgeAPISecret);
+        return requestURL;
+    }
+
+    private PaymentStatusRequest generatePaymentStatusRequest() {
+        PaymentStatusRequest req = new PaymentStatusRequest();
+        req.setReferenceNo("1234-abcd");
+        return req;
+    }
+
+    private AcademicBridgePaymentStatusResponse generatePaymentStatusResponse() {
+        AcademicBridgePaymentStatusResponse resp = new AcademicBridgePaymentStatusResponse();
+        resp.setAb_reference("1234-abcd");
+        resp.setError_code(0);
+        resp.setError_msg("No Error, Payment found");
+        resp.setSuccess(true);
+
+        return resp;
+    }
+
+    // Student details Helper methods
     private GetStudentDetailsResponse generateStudentDetailsResponse() {
         GetStudentDetailsResponse details = new GetStudentDetailsResponse();
         details.setBill_number("12345");
@@ -132,12 +189,13 @@ public class AcademicBridgeServiceTest {
         return details;
     }
 
+    // Generates relative url for getting student details
     private String generateGetStudentDetailsUrl(GetStudentDetails studentDetails) {
         String requestURL = String.format(getStudentDetailsURL,
                 studentDetails.getBillNumber(),
                 academicBridgeAPIKey,
                 academicBridgeAPISecret);
-        return baseUrl + requestURL;
+        return requestURL;
     }
 
     private void assertGetStudentResponseDetails (GetStudentDetailsResponse actual, GetStudentDetailsResponse expected) {
@@ -153,4 +211,59 @@ public class AcademicBridgeServiceTest {
         assertEquals(actual.getSchool_name(), expected.getSchool_name());
         // TODO: add success field
     }
+
+    public void configureWireMockForOkResponse(String expectedResponse, String url) {
+        // Mock of Academic Bridge API
+        wireMockRule.stubFor(WireMock.get(WireMock.urlPathEqualTo(url))
+                .willReturn(WireMock.aResponse()
+                        .withBody(expectedResponse)
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type",
+                                "application/json;charset=UTF-8")));
+    }
+
+    // Send payment helper methods
+    private void assertSendPaymentResponse(AcademicBridgeResponse expected, AcademicBridgeResponse actual) {
+        assertEquals(actual.getError_code(), expected.getError_code());
+        assertEquals(actual.getError_msg(), expected.getError_msg());
+        assertEquals(actual.getReference_number(), expected.getReference_number());
+        assertEquals(actual.isSuccess(), expected.isSuccess());
+    }
+
+    private String generateSavePaymentRequestUrl(SavePaymentRequest savePaymentRequest) {
+        System.out.println("======================> " + savePaymentURL);
+        String requestURL = String.format(savePaymentURL,
+                savePaymentRequest.getBillNumber(),
+                academicBridgeAPIKey,
+                academicBridgeAPISecret,
+                savePaymentRequest.getReferenceNo(),
+                "500",
+                savePaymentRequest.getSenderName(),
+                savePaymentRequest.getSenderPhoneNo(),
+                savePaymentRequest.getReason());
+        return requestURL;
+    }
+
+    private SavePaymentRequest generateSavePaymentRequestObj() {
+        SavePaymentRequest req = new SavePaymentRequest();
+        req.setBillNumber("12345");
+        req.setPaidAmount(500);
+        req.setReason("Bill");
+        req.setReferenceNo("1234-abcd");
+        req.setSenderName("John-Doe");
+        req.setSenderPhoneNo("112233");
+
+        return req;
+    }
+
+    private AcademicBridgeResponse generateSendPaymentResponse() {
+        AcademicBridgeResponse response = new AcademicBridgeResponse();
+        response.setError_code(0);
+        response.setError_msg("No error, payment is saved");
+        response.setReference_number("123ABC");
+        response.setSuccess(true);
+
+        return response;
+    }
+
 }
