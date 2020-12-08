d9b45610b73288655228b468580f3bf7cc4f1062
John Mulongo <john.mulongo@tracom.co.ke>
2020-12-08 07:58:20 +0300

    Update documentation info

--- src/main/java/co/ke/tracom/bprgatewaygen2/web/config/SwaggerConfig.java
+++ src/main/java/co/ke/tracom/bprgatewaygen2/web/config/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package co.ke.tracom.bprgatewaygen2.web.config;
 
+import springfox.documentation.service.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,6 +22,17 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Swagger Documentation for BPR gateway")
+                .description("")
+                .contact(new Contact("John Paul Mulongo",
+                        "",
+                        "john.mulongo@tracom.co.ke"))
+                .version("1.0.0")
                 .build();
     }
 
