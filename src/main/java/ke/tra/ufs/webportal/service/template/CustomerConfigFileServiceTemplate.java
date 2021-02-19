c7c7526050df2a93247ca00a0841e214f1e9149a
owori.juma@tracom.co.ke <tracom1234>
2021-02-19 13:54:20 +0300

    Parameter generation patch

--- src/main/java/ke/tra/ufs/webportal/service/template/CustomerConfigFileServiceTemplate.java
+++ src/main/java/ke/tra/ufs/webportal/service/template/CustomerConfigFileServiceTemplate.java
@@ -11,6 +11,7 @@ import ke.tra.ufs.webportal.service.UfsCustomerOutletService;
 import ke.tra.ufs.webportal.utils.SharedMethods;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.data.domain.Sort;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Field;
@@ -43,6 +44,7 @@ public class CustomerConfigFileServiceTemplate extends ParFileService implements
     }
 
     @Override
+    @Async
     public void generateCustomerFile(BigDecimal deviceId, String filePath) {
         TmsDevice tmsDevice = tmsDeviceRepository.findByDeviceIdAndIntrash(deviceId, "NO");
         if (tmsDevice == null) {
