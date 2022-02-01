e32dcd9da1b0a3fd2f8eb5673295416d98e87216
benson opisa <benson.onyino@tracom.co.ke>
2022-02-01 11:12:43 +0300

    cors configs for x-tenant and x-language headers

--- src/main/java/ke/co/tra/ufs/tms/config/multitenancy/MultiTenancyFilter.java
+++ src/main/java/ke/co/tra/ufs/tms/config/multitenancy/MultiTenancyFilter.java
@@ -3,6 +3,7 @@ package ke.co.tra.ufs.tms.config.multitenancy;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.*;
@@ -18,22 +19,33 @@ public class MultiTenancyFilter extends OncePerRequestFilter{
     protected void doFilterInternal(HttpServletRequest httpServletRequest,
                                     HttpServletResponse httpServletResponse,
                                     FilterChain filterChain) throws ServletException, IOException {
-        // Implement your logic to extract the Tenant Name here. Another way would be to
-        // parse a JWT and extract the Tenant Name from the Claims in the Token. In the
-        // example code we are just extracting a Header value:
-        System.err.println("== Calling multi-tenant filter ==");
-        String tenantName = httpServletRequest.getHeader("X-TenantID");
 
-        String language = httpServletRequest.getHeader("X-Language");
+        if (HttpMethod.OPTIONS.name().equalsIgnoreCase(httpServletRequest.getMethod())){
+            httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+            httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET,PATCH, OPTIONS, DELETE");
+            httpServletResponse.setHeader("Access-Control-Allow-Headers",
+                    "X-Requested-With, X-Auth-Token,Authorization,Content-Type," +
+                            "Content-Language,X-Language,X-TenantID,Accept,X-FORWARDED-FOR,X-CSRF-TOKEN,Accept-Language,Cache-control");
+            httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        }
+        else {
+            // Implement your logic to extract the Tenant Name here. Another way would be to
+            // parse a JWT and extract the Tenant Name from the Claims in the Token. In the
+            // example code we are just extracting a Header value:
+            System.err.println("== Calling multi-tenant filter ==");
+            String tenantName = httpServletRequest.getHeader("X-TenantID");
 
-        // Always set the Tenant Name, so we avoid leaking Tenants between Threads even in the scenario, when no
-        // Tenant is given. I do this because if somehow the afterCompletion Handler isn't called the Tenant Name
-        // could still be persisted within the ThreadLocal:
-        ThreadLocalStorage.setTenantName(tenantName);
-        ThreadLocalStorage.setLanguage(language);
+            String language = httpServletRequest.getHeader("X-Language");
 
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
+            // Always set the Tenant Name, so we avoid leaking Tenants between Threads even in the scenario, when no
+            // Tenant is given. I do this because if somehow the afterCompletion Handler isn't called the Tenant Name
+            // could still be persisted within the ThreadLocal:
+            ThreadLocalStorage.setTenantName(tenantName);
+            ThreadLocalStorage.setLanguage(language);
 
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
 
+        }
     }
 }
