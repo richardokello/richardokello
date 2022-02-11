0f52840ac5589bc0b3197c2e1b1fb1e9c29ba230
walter kivindu <walter.kivindu@tracom.co.ke>
2022-02-11 12:56:50 +0300

    Updated http-status to differentiate successful and failed transaction

--- src/main/java/co/ke/tracom/bprgateway/web/VisionFund/controller/VisionFundController.java
+++ src/main/java/co/ke/tracom/bprgateway/web/VisionFund/controller/VisionFundController.java
@@ -18,24 +18,36 @@ public class VisionFundController {
     @PostMapping(value = "deposit")
     public ResponseEntity<?> makeDeposit(@RequestBody AccountDepositRequest depositRequest){
         AccountDepositResponse depositResponse = fundService.makeDeposit(depositRequest);
+        if (depositResponse.getResponseCode().equals("05")){
+            return new ResponseEntity<>(depositResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
         return new ResponseEntity<>(depositResponse, HttpStatus.OK);
     }
 
     @PostMapping(value = "withdraw")
     public ResponseEntity<?> fundsWithdrawal(@RequestBody CashWithdrawalRequest withdrawalRequest){
         CashWithdrawalResponse withdrawalResponse = fundService.doWithdraw(withdrawalRequest);
+        if (withdrawalResponse.getResponseCode().equals("05")){
+            return new ResponseEntity<>(withdrawalResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
         return new ResponseEntity<>(withdrawalResponse, HttpStatus.OK);
     }
 
     @GetMapping(value = "verify")
     public ResponseEntity<?> accountVerification(@RequestBody CustomVerificationRequest verificationRequest){
         CustomVerificationResponse verificationResponse = fundService.verifyCustomer(verificationRequest);
+        if (verificationResponse.getResponseCode().equals("05")){
+            return new ResponseEntity<>(verificationResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
         return new ResponseEntity<>(verificationResponse, HttpStatus.OK);
     }
 
     @GetMapping(value = "balance")
     public ResponseEntity<?> accountBalance(@RequestBody BalanceEnquiryRequest enquiryRequest){
         BalanceEnquiryResponse enquiryResponse = fundService.getBalance(enquiryRequest);
+        if (enquiryResponse.getResponseCode().equals("05")){
+            return new ResponseEntity<>(enquiryResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
         return new ResponseEntity<>(enquiryResponse, HttpStatus.OK);
     }
 
