f1e5e363255f8fa74569c927bdee807353ef8b0c
walter kivindu <walter.kivindu@tracom.co.ke>
2022-03-07 11:12:24 +0300

    updated url from local to remote

--- src/main/java/co/ke/tracom/bprgateway/web/util/services/BaseServiceProcessor.java
+++ src/main/java/co/ke/tracom/bprgateway/web/util/services/BaseServiceProcessor.java
@@ -40,8 +40,8 @@ public class BaseServiceProcessor {
   //@Value("http://localhost:8787/authenticate/agent-deposit")
     private String agentDepositValidationUrl;
 
-    //@Value("http://192.168.24.30:8787/authenticate/authenticate-agent")
-    @Value("http://localhost:8787/authenticate/authenticate-agent")
+    @Value("http://192.168.24.30:8787/authenticate/authenticate-agent")
+    //@Value("http://localhost:8787/authenticate/authenticate-agent")
  private String agentpasswordValidationUrl;
 
     private final TransactionLimitManagerRepository transactionLimitManagerRepository;
